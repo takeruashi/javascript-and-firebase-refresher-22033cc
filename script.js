6d6cb2328d9be5a372c1c5cb9d5284fc583f9eeb
22033cc <22033cc@hvhs.school.nz>
2026-02-03 09:56:38 +1300

    second commit

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-var messageSpace = getElementbyId("welcomeMessage");
-messageSpace.innerHTML = "You've connected to the JavaScript!";
+
+/************************************************
+ * script.mjs
+ * main entry for index.html
+ * Written by Conor Church, Term 2 2025 
+ * 
+ * All variables to do with database written with fb_ but otherwise writen in camel case
+ * ***********************************************/
+console.log("hello sekai");
+var messageSpace;
+messageSpace = "You've connected to the JavaScript!";
+getElementbyId("welcomeMessage").innerHTML = messageSpace;
+/*
+= "#1     " + LeaderBoard[0].display_Name +"-"+LeaderBoard[0].high_Score_PES 
+(*/
